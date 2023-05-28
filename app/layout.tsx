aa9874e6e97e385f59c8c5035e137fcd0fef6908
Joshua Tan <joshua.tan@hytechc.com>
2023-05-28 23:50:36 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import "./globals.css";
+import Modal from "@/components/Modal";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Welcome to Next.js",
+};
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-[#F5F6F8]">
+        {children}
+        <Modal />
+      </body>
+    </html>
+  );
+}
